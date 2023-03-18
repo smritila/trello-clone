315d49a0cd423b03afbefc563bfc1a534537edf4
Smriti <smrtghoshal24@gmail.com>
2023-03-18 22:25:56 +0530

    chore: move state management in add nee form

--- src/components/ListAddNewForm.js
+++ src/components/ListAddNewForm.js
@@ -1,13 +1,34 @@
-function ListAddNewForm(props) {
-  return (
-    <div className="add-new-form">
-      <textarea rows="5" placeholder="Enter a little for this card"></textarea>
-      <button className="btn-list-card">Add Card</button>
-      <button className="btn-list-card" onClick={props.hideForm}>
-        Cancel
+import { useState } from "react";
+
+function ListAddNewForm() {
+  const [isVisibleForm, toggleFormVisibility] = useState(false);
+
+  let dynamicAddNewTemplate = null;
+  if (isVisibleForm) {
+    dynamicAddNewTemplate = (
+      <div className="add-new-form">
+        <textarea
+          rows="5"
+          placeholder="Enter a little for this card"
+        ></textarea>
+        <button className="btn-list-card">Add Card</button>
+        <button
+          className="btn-list-card"
+          onClick={() => toggleFormVisibility(false)}
+        >
+          Cancel
+        </button>
+      </div>
+    );
+  } else {
+    dynamicAddNewTemplate = (
+      <button className="btn" onClick={() => toggleFormVisibility(true)}>
+        Add a card
       </button>
-    </div>
-  );
+    );
+  }
+
+  return dynamicAddNewTemplate;
 }
 
 export default ListAddNewForm;
